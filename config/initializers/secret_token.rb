132cf6397e2462337e4e5e53238e3f13100c805f
David Genord II <david@collectiveidea.com>
2013-02-03 12:51:03 -0500

    Base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bender::Application.config.secret_token = '4e4638ef83a21bc80dfaee4fc08479909a621f580c2935f4954ab48bc7be46d165817006aa5d884b2b0f93eaa499fcdf86bc6609739c2d021e90d90fbab0b9d9'
