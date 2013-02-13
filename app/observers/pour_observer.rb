267f6b8edcdd9b952050f6c1a16cd5839fc5a16f
David Genord II <david@collectiveidea.com>
2013-02-13 16:57:19 -0500

    Move and activate the pour observer

--- app/observers/pour_observer.rb
+++ app/observers/pour_observer.rb
@@ -1,9 +1,9 @@
 class PourObserver < ActiveRecord::Observer
-	attr_accessor :faye_client
+  attr_accessor :faye_client
   observe :pour
 
   def after_update(pour)
-  	puts "Pour update"
+    puts "Pour update"
     message = {channel: "/testing", data: {type: "pour_volume", tap_id: pour.keg.beer_tap.id, volume: pour.volume.to_s}}
     uri = URI.parse("http://localhost:9292/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
