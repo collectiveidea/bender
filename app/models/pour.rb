c369ac26b8c6a171ef111e03c635829de455a891
Tres Trantham <tres@trestrantham.com>
2013-02-17 23:47:21 -0500

    Opened model attributes to allow for various updates

--- app/models/pour.rb
+++ app/models/pour.rb
@@ -6,6 +6,8 @@
 # ml * 0.033814 = fl oz
 
 class Pour < ActiveRecord::Base
+	attr_accessible :user_id, :change_type, :sensor_ticks, :volume, :started_at, :finished_at
+
   belongs_to :keg
   belongs_to :user
 end
