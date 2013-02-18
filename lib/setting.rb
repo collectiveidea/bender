5d0a7f9f90ba46fad08d3f8453b611036b2f4c0b
Tres Trantham <tres@trestrantham.com>
2013-02-17 23:45:11 -0500

    Added pour_timeout to Settings

--- lib/setting.rb
+++ lib/setting.rb
@@ -3,6 +3,10 @@ module Setting
     settings['hubot_url']
   end
 
+  def self.pour_timeout
+  	settings['pour_timeout']
+  end
+
   def self.settings
     @settings ||= YAML.load_file(Rails.root.join('config','settings.yml'))
   end
