3e602c9eb7c52e0173ec21b967ce1a40d8ea1241
David Genord II <david@collectiveidea.com>
2013-03-01 14:49:13 -0500

    Only display pours that have a volume

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,6 @@
 class HomepageController < ApplicationController
   def index
     @beer_taps = BeerTap.all
-    @pours = Pour.order('created_at desc').limit(@beer_taps.size * 3)
+    @pours = Pour.where('volume IS NOT NULL').order('created_at desc').limit(@beer_taps.size * 3)
   end
 end
