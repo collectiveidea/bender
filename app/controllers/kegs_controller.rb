82bc0eac99904068a3c2d7733a9feb787883eee6
David Genord II <david@collectiveidea.com>
2013-03-01 14:54:56 -0500

    Cleanup kegs controller

--- app/controllers/kegs_controller.rb
+++ app/controllers/kegs_controller.rb
@@ -1,13 +1,13 @@
 class KegsController < ApplicationController
-	respond_to :html, :json
+  respond_to :html, :json
 
-	def index
-		@kegs = Keg.where('active = ?', true)
-		respond_with @kegs
-	end
+  def index
+    @kegs = Keg.where(active: true)
+    respond_with @kegs
+  end
 
-	def show
-		@keg = Keg.find(params[:id])
-		respond_with @keg
-	end
+  def show
+    @keg = Keg.find(params[:id])
+    respond_with @keg
+  end
 end
