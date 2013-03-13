8082e0187ac9d2d1baf772f862c366c8f4b1045a
Tres Trantham <tres@trestrantham.com>
2013-03-12 20:23:45 -0400

    Add API endpoint for User create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,17 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:success] = 'User created'
-      redirect_to root_path
+      respond_to do |format|
+        format.json { respond_with @user }
+        format.html do
+          flash[:success] = 'User created'
+          redirect_to root_path
+        end
+      end
     else
       render :new
     end
