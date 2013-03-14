f0fa01423072eba4d35cbfd8dcc2944b837c057b
Tres Trantham <tres@trestrantham.com>
2013-03-14 08:42:53 -0400

    Refactor User last_pour_at

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
-  attr_reader :last_pour_at
 
   validates :name, :uniqueness => true
   
   def last_pour_at
-    pour = Pour.where('volume IS NOT NULL AND user_id = ?', self.id).order('created_at desc').first
-    pour.nil? ? nil : pour.created_at
+    Pour.where('volume IS NOT NULL AND user_id = ?', self.id).maximum(:created_at)
   end
 end
