8510c678db88e633b080d1a83d12c108c4862b78
David Genord II <david@collectiveidea.com>
2014-02-13 22:06:06 -0500

    Add a guest user lookup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   validates :name, :uniqueness => true
 
+  def self.guest
+    find_by(id: 0) || new(id: 0)
+  end
+
   def last_pour_at
     Pour.where('volume IS NOT NULL AND user_id = ?', self.id).maximum(:created_at)
   end
