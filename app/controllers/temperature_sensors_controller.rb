92ad1660de82170a22afdcfd1749ba112a666781
David Genord II <david@collectiveidea.com>
2015-11-11 22:53:20 -0500

    Data is short lived set appropriate headers
    This should also skip rack cache

--- app/controllers/temperature_sensors_controller.rb
+++ app/controllers/temperature_sensors_controller.rb
@@ -1,5 +1,7 @@
 class TemperatureSensorsController < ApplicationController
   def readings
+    expires_now
+
     sensor = TemperatureSensor.find(params[:sensor_id])
     start_time = Time.at(params[:start_timestamp].to_i)
     end_time   = start_time + params[:duration].to_i
