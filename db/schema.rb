4b9985080ef7f59650af74b6a4eee35a52537eaa
Victoria Gonda <v.m.gonda@gmail.com>
2017-08-15 14:21:44 -0400

    Add RFID to user
    Users should be able to sign in, and create a pour using an rfid scan.
    
    [[Contributes #147199951]](https://www.pivotaltracker.com/story/show/147199951)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025025034) do
+ActiveRecord::Schema.define(version: 20170815181511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20161025025034) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "email"
+    t.string   "rfid"
   end
 
 end
