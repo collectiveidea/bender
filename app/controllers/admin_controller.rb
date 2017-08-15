42b2829e61deda3e6ce1db3c87495f2d93108780
Victoria Gonda <v.m.gonda@gmail.com>
2017-08-15 10:59:49 -0400

    Add basic authentication for admin actions
    We have the authentication conditional to if there is a password set. We don't
    want to have to log in if there is not a password. There is not a password for
    our local configuration, so we don't want the authentication in that case.
    
    [[Finishes #149859218]](https://www.pivotaltracker.com/story/show/149859218)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,9 @@
 class AdminController < ApplicationController
+  http_basic_authenticate_with name: "bender", password: Setting.admin_password, if: :needs_authetication?
+
+  private
+
+  def needs_authetication?
+    Setting.admin_password.present?
+  end
 end
