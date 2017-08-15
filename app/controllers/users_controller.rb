e85e276bec38265a92c5d440f8f8496be8b46f6a
Victoria Gonda <v.m.gonda@gmail.com>
2017-08-15 16:20:52 -0400

    Allow an admin to delete a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,6 +54,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
+  end
+
   protected
 
   def user_params
