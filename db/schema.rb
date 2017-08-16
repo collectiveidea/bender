bccb881694b80d42e9288c789da6425b087b0c7a
Victoria Gonda <v.m.gonda@gmail.com>
2017-08-16 10:57:53 -0400

    Add credits to user
    By having the number of credits set on a user, we are able to optionally limit
    the number of pints a user can pour in a month.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815181511) do
+ActiveRecord::Schema.define(version: 20170816144714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20170815181511) do
     t.datetime "updated_at", null: false
     t.string   "email"
     t.string   "rfid"
+    t.decimal  "credits"
   end
 
 end
