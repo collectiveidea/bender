d01b5dfd16af344f286acaaac7478afc16ef063d
Daniel Morrison <daniel@collectiveidea.com>
2022-03-28 13:44:22 -0400

    Actually use headers in spec

--- spec/requests/api/webhooks/optix_spec.rb
+++ spec/requests/api/webhooks/optix_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Optix Webhooks" do
     describe "receiving a checkin" do
       it "creates a user if needed" do
         headers = {"CONTENT_TYPE" => "application/json"}
-        post "/api/v1/webhooks/optix", params: '{"check_in_datetime": "2022-03-23 16:12", "check_in_timestamp": "1648051960", "checkin_id": "216292", "created_datetime": "2022-03-23 16:12", "member_email": "member@example.com", "member_full_name": "Sam Body", "member_last_name": "Body", "member_name": "Sam", "venue_id": "24263", "venue_name": "BizCo HQ", "workspace_name": "BizCo"}'
+        post "/api/v1/webhooks/optix", params: '{"check_in_datetime": "2022-03-23 16:12", "check_in_timestamp": "1648051960", "checkin_id": "216292", "created_datetime": "2022-03-23 16:12", "member_email": "member@example.com", "member_full_name": "Sam Body", "member_last_name": "Body", "member_name": "Sam", "venue_id": "24263", "venue_name": "BizCo HQ", "workspace_name": "BizCo"}', headers: headers
 
         expect(response.status).to eq(200)
         user = User.find_by(email: "member@example.com")
@@ -16,7 +16,7 @@ RSpec.describe "Optix Webhooks" do
         user = User.create!(email: "member@example.com", name: "")
 
         headers = {"CONTENT_TYPE" => "application/json"}
-        post "/api/v1/webhooks/optix", params: '{"check_in_datetime": "2022-03-23 16:12", "check_in_timestamp": "1648051960", "checkin_id": "216292", "created_datetime": "2022-03-23 16:12", "member_email": "member@example.com", "member_full_name": "Sam Body", "member_last_name": "Body", "member_name": "Sam", "venue_id": "24263", "venue_name": "BizCo HQ", "workspace_name": "BizCo"}'
+        post "/api/v1/webhooks/optix", params: '{"check_in_datetime": "2022-03-23 16:12", "check_in_timestamp": "1648051960", "checkin_id": "216292", "created_datetime": "2022-03-23 16:12", "member_email": "member@example.com", "member_full_name": "Sam Body", "member_last_name": "Body", "member_name": "Sam", "venue_id": "24263", "venue_name": "BizCo HQ", "workspace_name": "BizCo"}', headers: headers
 
         expect(response.status).to eq(200)
         user.reload
@@ -27,7 +27,7 @@ RSpec.describe "Optix Webhooks" do
         user = User.create!(email: "member@example.com", name: "Original Name")
 
         headers = {"CONTENT_TYPE" => "application/json"}
-        post "/api/v1/webhooks/optix", params: '{"check_in_datetime": "2022-03-23 16:12", "check_in_timestamp": "1648051960", "checkin_id": "216292", "created_datetime": "2022-03-23 16:12", "member_email": "member@example.com", "member_full_name": "Sam Body", "member_last_name": "Body", "member_name": "Sam", "venue_id": "24263", "venue_name": "BizCo HQ", "workspace_name": "BizCo"}'
+        post "/api/v1/webhooks/optix", params: '{"check_in_datetime": "2022-03-23 16:12", "check_in_timestamp": "1648051960", "checkin_id": "216292", "created_datetime": "2022-03-23 16:12", "member_email": "member@example.com", "member_full_name": "Sam Body", "member_last_name": "Body", "member_name": "Sam", "venue_id": "24263", "venue_name": "BizCo HQ", "workspace_name": "BizCo"}', headers: headers
 
         expect(response.status).to eq(200)
         user.reload
@@ -38,7 +38,7 @@ RSpec.describe "Optix Webhooks" do
     describe "receiving a new member" do
       it "creates a user if needed" do
         headers = {"CONTENT_TYPE" => "application/json"}
-        post "/api/v1/webhooks/optix", params: '{"city": "", "email": "member@example.com", "first_name": "", "full_name": " ", "last_name": "", "member_id": "161128", "phone": "", "venue_id": "24263", "venue_name": "BizCo"}'
+        post "/api/v1/webhooks/optix", params: '{"city": "", "email": "member@example.com", "first_name": "", "full_name": " ", "last_name": "", "member_id": "161128", "phone": "", "venue_id": "24263", "venue_name": "BizCo"}', headers: headers
 
         expect(response.status).to eq(200)
         user = User.find_by(email: "member@example.com")
@@ -49,7 +49,7 @@ RSpec.describe "Optix Webhooks" do
         user = User.create!(email: "member@example.com", name: "Original Name")
 
         headers = {"CONTENT_TYPE" => "application/json"}
-        post "/api/v1/webhooks/optix", params: '{"city": "", "email": "member@example.com", "first_name": "", "full_name": " ", "last_name": "", "member_id": "161128", "phone": "", "venue_id": "24263", "venue_name": "BizCo"}'
+        post "/api/v1/webhooks/optix", params: '{"city": "", "email": "member@example.com", "first_name": "", "full_name": " ", "last_name": "", "member_id": "161128", "phone": "", "venue_id": "24263", "venue_name": "BizCo"}', headers: headers
 
         expect(response.status).to eq(200)
         user = User.find_by(email: "member@example.com")
